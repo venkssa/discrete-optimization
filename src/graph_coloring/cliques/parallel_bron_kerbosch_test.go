40f8bb2f52e8f9cf7dab3b0e5e934ea051c5867e
Venkatesh Amudhan <venks.sa@gmail.com>
2016-09-30 00:45:18 -0700

    Fix broken test

--- src/graph_coloring/cliques/parallel_bron_kerbosch_test.go
+++ src/graph_coloring/cliques/parallel_bron_kerbosch_test.go
@@ -5,49 +5,6 @@ import (
 	"graph_coloring/testdata"
 )
 
-func TestWorkQueue_GetWork_WhenNoWorkShouldReturnAnError(t *testing.T) {
-	q := NewWorkQueue()
-	work, err := q.GetWork()
-
-	if err == nil {
-		t.Errorf("Expected an error but got %v", work)
-	}
-	t.Log(work, err)
-}
-
-func TestWorkQueue_GetWork(t *testing.T) {
-	q := NewWorkQueue()
-	expectedWork := Work{VIdx: 1}
-
-	q.AddToQueue(expectedWork)
-
-	actualWork, err := q.GetWork()
-
-	if err != nil {
-		t.Fatalf("Expected work but got %v", err)
-	}
-
-	if actualWork.VIdx != expectedWork.VIdx {
-		t.Fatalf("Expected %v but got %v", expectedWork, actualWork)
-	}
-}
-
-func TestWorkQueue_GetWork_SecondTimeWithNoWorkShouldReturnAnError(t *testing.T) {
-	q := NewWorkQueue()
-
-	q.AddToQueue(Work{VIdx: 1})
-
-	_, err := q.GetWork()
-	if err != nil {
-		t.Fatalf("Expected work but got %v", err)
-	}
-
-	_, err = q.GetWork()
-	if err == nil {
-		t.Fatal("Expected an error but did not get one")
-	}
-}
-
 func TestParallelBKAlgo_FindAllMaximalCliques(t *testing.T) {
 	g := testdata.Graph(testdata.GC_20_3)
 	expectedCliques :=  BronKerbosch().FindAllMaximalCliques(g).Cliques
